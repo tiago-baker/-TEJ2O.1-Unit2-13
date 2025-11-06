dd08936725b7738f6d3e4361e56eaf32feceb418
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-11-05 22:47:52 -0500

    save

--- main.ts
+++ main.ts
@@ -5,66 +5,73 @@
  * This program counts down
  */
 
+/* Copyright (c) 2020 MTHS All rights reserved
+ *
+ * Created by: Tiago Baker
+ * Created on: Oct 2025
+ * This program counts down
+*/
+
 // variables
-let counter1 = 4;
-let neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB);
+let counter1 = 4
+let neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
 // count down using neopixels
 input.onButtonPressed(Button.A, function () {
     // when counter1 = 4
     if (counter1 == 4) {
         // show 4 neopixels
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red));
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red));
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red));
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red));
-        neopixelStrip.show();
-        basic.pause(1000);
-        counter1 = counter1 - 1;
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+        basic.pause(1000)
+        counter1 = counter1 - 1
     }
     // when counter1 = 3
     if (counter1 == 3) {
         // show 3 neopixels
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red));
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red));
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red));
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black));
-        neopixelStrip.show();
-        basic.pause(1000);
-        counter1 = counter1 - 1;
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.pause(1000)
+        counter1 = counter1 - 1
     }
     // when counter1 = 2
     if (counter1 == 2) {
         // show 2 neopixels
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red));
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red));
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black));
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black));
-        neopixelStrip.show();
-        basic.pause(1000);
-        counter1 = counter1 - 1;
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.pause(1000)
+        counter1 = counter1 - 1
     }
     // when counter1 = 1
     if (counter1 == 1) {
         // show one neopixel
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red));
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black));
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black));
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black));
-        neopixelStrip.show();
-        basic.pause(1000);
-        counter1 = counter1 - 1;
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.pause(1000)
+        counter1 = counter1 - 1
     }
     // when counter1 = 0
     if (counter1 == 0) {
         // show no neopixels
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black));
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black));
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black));
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black));
-        neopixelStrip.show();
-        basic.pause(1000);
-        // reset counter1
-        counter1 = 4;
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.pause(1000)
+        // reset counter1 
+        counter1 = 4
     }
-});
+})
